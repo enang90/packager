95a502fcafa4bd0eb5fcc5139c554db7f22952bf
Matthias Vandermaesen <matthias@colada.be>
2010-08-12 22:50:00 +0200

    acl integration + licensing + versioning

---
+++ app/controllers/versions_controller.php
@@ -0,0 +1,13 @@
+<?php
+class VersionsController extends AppController {
+	var $view = 'Theme';
+	var $theme = 'private';
+  var $name = 'Versions';
+  var $components = array('Session');
+  var $uses = array('User', 'Brand');
+
+  function add() {
+	
+  }
+
+}
