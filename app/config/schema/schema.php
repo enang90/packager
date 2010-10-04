df1b6d59120d4a30c81e06feffff6d2002581db5
Matthias Vandermaesen <matthias@colada.be>
2010-10-04 22:05:34 +0200

    config changes

---
+++ app/config/schema/schema.php
@@ -0,0 +1,108 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* App schema generated on: 2010-09-26 23:09:04 : 1285535704*/
+class AppSchema extends CakeSchema {
+	var $name = 'App';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $acos = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'alias' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'lft' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'rght' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+	var $aros = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'alias' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'lft' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'rght' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+	var $aros_acos = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'aro_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'index'),
+		'aco_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
+		'_create' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2),
+		'_read' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2),
+		'_update' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2),
+		'_delete' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'ARO_ACO_KEY' => array('column' => array('aro_id', 'aco_id'), 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+	var $brands = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 155),
+		'icon' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'created' => array('type' => 'timestamp', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'),
+		'subscription_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'owner' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20),
+		'job_created' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 6),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+	var $brands_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20, 'key' => 'primary'),
+		'brand_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 20),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+	var $subscriptions = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 155),
+		'amount' => array('type' => 'float', 'null' => false, 'default' => NULL),
+		'term' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 155),
+		'period' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'active' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+	var $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'first_name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'last_name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40),
+		'email' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 40),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'blocked' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 1),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+	var $versions = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'brand_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'inittime' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'version_major' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'version_minor' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'version_build' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'source_official_tag' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'source_type' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'source_git_url' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'name_safe' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'company' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'homepage_url' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'support_url' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'info_url' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'status' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+}
+?>
