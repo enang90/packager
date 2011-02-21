4d81a53707c452a17a17e16145a29c412456275f
Matthias Vandermaesen <matthias@colada.be>
2011-02-21 05:19:16 +0100

    added: comments in routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -21,33 +21,44 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+
 /**
  * Here, we are connecting '/' (base path) to controller called 'Pages',
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
-//	Router::connect('/', array('controller' => 'Dashboard', 'action' => 'index', 'home'));
+  //	Router::connect('/', array('controller' => 'Dashboard', 'action' => 'index', 'home'));
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
-/* Reroute versions to Appcasting */
+/** 
+ *  Reroute versions to Appcasting
+ */
   Router::connect('/appcasting', array('controller' => 'versions', 'action' => 'index'));
   Router::connect('/appcasting/add', array('controller' => 'versions', 'action' => 'add'));
   Router::connect('/appcasting/archive', array('controller' => 'versions', 'action' => 'archive'));
 
-/* Paypal IPN plugin */
+/**
+ * Paypal IPN plugin
+ */
   Router::connect('/paypal_ipn/process', array('plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'process'));
 
-/* Optional Routes, but nice for administration */
+/**
+ * Optional Routes, but nice for administration 
+ */
   Router::connect('/paypal_ipn/edit/:id', array('admin' => true, 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'edit'), array('id' => '[a-zA-Z0-9\-]+', 'pass' => array('id')));
   Router::connect('/paypal_ipn/view/:id', array('admin' => true, 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'view'), array('id' => '[a-zA-Z0-9\-]+', 'pass' => array('id')));
   Router::connect('/paypal_ipn/delete/:id', array('admin' => true, 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'delete'), array('id' => '[a-zA-Z0-9\-]+', 'pass' => array('id')));
   Router::connect('/paypal_ipn/add', array('admin' => true, 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'edit'));
   Router::connect('/paypal_ipn', array('admin' => true, 'plugin' => 'paypal_ipn', 'controller' => 'instant_payment_notifications', 'action' => 'index'));/*
-/* End Paypal IPN plugin */
 
+
+/**
+ * Provide hudson an endpoint to pull new version information
+ */
  Router::connect('/versions/hudson', array('controller' => 'versions', 'action' => 'hudson', '[method]' => 'POST'));
  Router::parseExtensions();
