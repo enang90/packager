9cc8fc84752ecf5b49d3b2ed6f9d1d45946d59e9
Matthias Vandermaesen <matthias@colada.be>
2011-02-28 09:23:29 +0100

    Added: check if a brand was already activated and paid for...

--- app/controllers/subscriptions_controller.php
+++ app/controllers/subscriptions_controller.php
@@ -18,15 +18,20 @@ class SubscriptionsController extends AppController {
       $this->redirect(array('controller'=> 'brands', 'action' => 'index'));
 		}
 		
-	  $subscription = $this->Subscription->find('first');
+	  $transaction = ClassRegistry::init('PaypalIpn.InstantPaymentNotification')->findById($brand['subscription_id']);
+	  
+	  if ($transaction) {
+	    $this->render('has_subscription');
+	  }
 
+	  $subscription = $this->Subscription->find('first');
 	  $button = array(
 		  'test' => TRUE,
 		  'type' => $subscription['Subscription']['type'], 
 	    'amount' => $subscription['Subscription']['amount'],
 	    'term' => $subscription['Subscription']['term'],
 	    'period' => $subscription['Subscription']['period'],
-	    'item_name' => __(sprintf('Subscription plan for \'%s\'', $brand['name']), TRUE),
+	    'item_name' => __(sprintf('Subscription plan for brand: %s', $brand['name']), TRUE),
 			'item_number' => $brand['id']);
 			
 		$this->set('brand_name', $brand['name']);	
