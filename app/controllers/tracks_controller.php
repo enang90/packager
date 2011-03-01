286c1b65704c85ace612d8d50e47903273339f37
Matthias Vandermaesen <matthias@colada.be>
2011-03-01 05:42:13 +0100

    Added: authorization to tracks + check if an brand is active

--- app/controllers/tracks_controller.php
+++ app/controllers/tracks_controller.php
@@ -26,10 +26,15 @@ class TracksController extends AppController {
     $brand = $this->Session->read('Brand');
     $this->Brand->read(NULL, $brand['id']);
     
+    // limit access to feature until activated
+    if (!($brand = $this->_getBrand())) {
+      $this->redirect(array('controller' => 'brands', 'action' => 'index'));
+    }
+
     if (!$this->Track->id) {
       $this->Track->id = $this->Brand->data['Track']['id'];
     }
-    
+
     $this->data = $this->Track->read();
     
     // retrieve data to generate the publishing updates form
@@ -41,8 +46,29 @@ class TracksController extends AppController {
       }
     }
 
-
     $this->set('versions', $versions);
     $this->set('brand', $this->Brand->data['Brand']);
+  }
+
+  /**
+   * Retrieve the brand data from the session
+   * @return $brand mixed FALSE if not found/inactive, otherwise: an array with brand data
+   */
+  function _getBrand() {
+    $brand = $this->Session->read('Brand');
+
+    // brand not found
+    if (!$brand) {
+      $this->_flash(__('We could not find a brand associated with your session. Please switch to a brand through the brand selector.', TRUE), 'pandion');
+      $brand = FALSE;
+    }
+
+    // brand not active
+    if (!$brand['active']) {
+      $this->_flash(sprintf(__('The current brand \'%s\' is not activated. You need a subscription plan before you can proceed using our features.', TRUE), $brand['name']), 'pandion');
+      $brand = FALSE;
+    }
+
+    return $brand;
   }
 }
